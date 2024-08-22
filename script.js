2c76695366cef848ffbd06f8538ac0af6e94d022
lorrayne15 <101271186+lorrayne15@users.noreply.github.com>
2024-08-22 11:47:13 -0300

    pronto

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Enunciado principal",
+        enunciado: "Qual é o objetivo principal do vôlei?",
         alternativas: [
             {
-                texto: "Texto 1",
-                afirmacao: "Afirmação 1 "
+                texto: "Marcar pontos ao derrubar a bola no chão do adversário.",
+                afirmacao: "O objetivo principal do vôlei é fazer com que a bola toque o chão do time adversário para marcar pontos."
             },
             {
-                texto: "Texto 2",
-                afirmacao: "Afirmação 2"
+                texto: "Evitar que a bola toque o chão do seu próprio lado",
+                afirmacao: "Para vencer no vôlei, é crucial impedir que a bola caia no seu próprio lado da quadra."
             }
         ]
     },
     {
-        enunciado: "Enunciado 2",
+        enunciado: "Qual é o número de jogadores em cada time durante uma partida de vôlei?",
         alternativas: [
             {
-                texto: "texto botão 1",
-                afirmacao: "Afirmação do texto botão 2"
+                texto: "Seis jogadores por time",
+                afirmacao: "Em uma partida de vôlei, cada equipe é composta por seis jogadores que se revezam nas posições de ataque e defesa."
             },
             {
-                texto: "texto botão 2",
-                afirmacao: "Afirmação do texto botão 2"
+                texto: " Quatro jogadores por time",
+                afirmacao: "O vôlei com quatro jogadores por time não é a regra padrão, mas é uma variação conhecida como vôlei de praia em algumas competições."
             }
         ]
     },
     {
-        enunciado: "Enunciado 3",
+        enunciado: "O que caracteriza uma bomba no vôlei?",
         alternativas: [
             {
-                texto: "texto botão 1",
-                afirmacao: "Afirmação do texto botão 1"
+                texto: " Um ataque forte e rápido",
+                afirmacao: "Uma bomba no vôlei refere-se a um ataque poderoso e rápido que dificulta a defesa do time adversário."
             },
             {
-                texto: "texto botão 2",
-                afirmacao: "Afirmação do texto botão 2"
+                texto: "Um bloqueio bem-sucedido",
+                afirmacao: "O termo bomba não é geralmente usado para descrever um bloqueio, mas sim um ataque forte."
             }
         ]
     },
     {
-        enunciado: "Enunciado 4",
+        enunciado: "Qual é a função principal do levantador em um time de vôlei?",
         alternativas: [
             {
-                texto: "texto botão 1",
-                afirmacao: "Afirmação do texto botão 1"
+                texto: "Organizar as jogadas e preparar os ataques",
+                afirmacao: "O levantador é responsável por organizar as jogadas e fornecer passes precisos para os atacantes."
             },
             {
-                texto: "texto botão 2",
-                afirmacao: "Afirmação do texto botão 2"
+                texto: "Defender e receber os ataques adversários",
+                afirmacao: "A principal função de defender e receber ataques adversários não é do levantador, mas dos jogadores de defesa."
             }
         ]
     },
     {
-        enunciado: "Enunciado 5",
+        enunciado: "Qual é o propósito do toque de dedos no vôlei?",
         alternativas: [
             {
-                texto: "texto botão 1",
-                afirmacao: "Afirmação do texto botão 1"
+                texto: "Realizar passes precisos e controlar a bola",
+                afirmacao: "O toque de dedos é usado para fazer passes precisos e controlar a trajetória da bola durante o jogo."
             },
             {
-                texto: "texto botão 2",
-                afirmacao: "Afirmação do texto botão 2"
+                texto: "Atacar diretamente o adversário",
+                afirmacao: "O toque de dedos não é usado para atacar diretamente, mas para passar e organizar o jogo."
             }
         ]
     },
